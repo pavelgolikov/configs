2d6f9af1d41d133711147b81f740d03ed583ccae
Pavel Golikov <golikovp@eco-18.syslab.sandbox>
2020-04-14 11:58:44 -0400

    updated get_new

--- get_new.sh
+++ get_new.sh
@@ -1,4 +1,11 @@
 cp .vimrc ../.vimrc
 cp .tmux.conf ../.tmux.conf
-cp config ../.ssh/config
 cp .ideavimrc ../.ideavimrc
+
+cat bashrc_additions >> ../.bashrc
+
+cd ~/
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
