d2dc8993078f141cd4800cafe8cc812d8a8c5d71
Pavel Golikov <paulgolikov@gmail.com>
2026-08-06 20:53:00 -0400

    removed stale files; input remapper json

--- map_tablet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-id=$(xinput | grep VEIKK | grep pointer | awk '{print $5}' | cut -c4-6)
-
-xinput map-to-output $id DP-5
--- renew_configs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp vimrc ~/.vimrc
-cp tmux.conf ~/.tmux.conf
-cp mybindings ~/.mybindings
-
-cat bashrc_additions >> ~/.bashrc
